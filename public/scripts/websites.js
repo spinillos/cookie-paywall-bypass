53e62ee1081a3b7ca753221e93583aa282b5182b
spinillos <selenepinillos@gmail.com>
2024-03-25 19:15:28 +0100

    Remove eurpapress style inside block

--- public/scripts/websites.js
+++ public/scripts/websites.js
@@ -27,11 +27,12 @@ const genericCheck = function () {
     // EuropaPress
     let needsClick = document.querySelector(".needsclick")
     if (needsClick) {
+        document.head.getElementsByTagName("style")[0].remove()
         needsClick.remove()
     }
-    document.head.getElementsByTagName("style")[0].remove()
 
     document.body.classList.remove("_y9ev9r")
+    document.body.classList.remove("sxnlzit")
     document.body.classList.remove("bodyBlocked")
 }
 
